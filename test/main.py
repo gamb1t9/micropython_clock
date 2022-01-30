eae32f2809b47d587412648b300cfd948bc3dc62
gamb1t <rawshiet@gmail.com>
2022-01-30 19:40:21 +0100

    nothing too fancy

--- test/main.py
+++ test/main.py
@@ -1,10 +1,18 @@
 import time
-from machine import Pin
+import machine
+import senko #ota
+
+def ota_init():
+    OTA = senko.Senko(user="gamb1t9", repo="micropython_clock", working_dir="test", files = ["boot.py", "main.py"])
+    if OTA.update():
+        print("Updated to the latest version! Rebooting...")
+        machine.reset()
 
 led=Pin(2,Pin.OUT)
 
-while True:
+for i in range(0,20):
     led.value(1)
-    time.sleep(0.5)
+    time.sleep(0.3)
     led.value(0)
-    time.sleep(0.5)
+    time.sleep(0.3)
+
