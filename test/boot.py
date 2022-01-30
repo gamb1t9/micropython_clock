36919dd72ff4ac2a29c5599f4a95c52be5c79ce2
gamb1t <rawshiet@gmail.com>
2022-01-30 17:18:03 +0100

    idk

---
+++ test/boot.py
@@ -0,0 +1,10 @@
+import network
+import time
+# enable station interface and connect to WiFi access point
+nic = network.WLAN(network.STA_IF)
+nic.active(True)
+nic.ifconfig(('192.168.0.250', '255.255.255.0', '192.168.0.1', '8.8.8.8'))
+nic.connect('WONDERLAND', 'Kiskutya9')
+time.sleep(3)
+if nic.isconnected():
+    print("connected. IPs: " +str(nic.ifconfig()))
