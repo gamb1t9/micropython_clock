590608f1d62ca25b50168c381f71b72138fe20db
gamb1t <rawshiet@gmail.com>
2022-01-30 19:41:42 +0100

    nothing too fancy2

--- test/main.py
+++ test/main.py
@@ -2,14 +2,21 @@ import time
 import machine
 import senko #ota
 
-def ota_init():
-    OTA = senko.Senko(user="gamb1t9", repo="micropython_clock", working_dir="test", files = ["boot.py", "main.py"])
+OTA = senko.Senko(user="gamb1t9", repo="micropython_clock", working_dir="test", files = ["boot.py", "main.py"])
+    
+def ota_pull():
     if OTA.update():
         print("Updated to the latest version! Rebooting...")
         machine.reset()
 
-led=Pin(2,Pin.OUT)
+def ota_fetch():
+    if OTA.fetch()
+        print("There are available updates on the master")
 
+
+ota_fetch()
+
+led=Pin(2,Pin.OUT)
 for i in range(0,20):
     led.value(1)
     time.sleep(0.3)
