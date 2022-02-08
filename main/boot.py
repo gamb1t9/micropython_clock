792421998a4d5472872f857623c69247d5f66819
gamb1t <rawshiet@gmail.com>
2022-02-08 18:14:57 +0100

    moved OTA to boot

--- main/boot.py
+++ main/boot.py
@@ -1,5 +1,24 @@
 import network
 import time
+import senko #ota
+import machine
+
+OTA = senko.Senko(user="gamb1t9", repo="micropython_clock", branch="develop", working_dir="main", files = ["boot.py", "main.py", "test/time.py"])
+    
+def ota_pull():
+    if OTA.update():
+        print("Updated to the latest version! Rebooting...")
+        machine.reset()
+
+def ota_fetch():
+    if OTA.fetch():
+        print("There are available updates on the DEVELOP branch")
+        ota_pull()
+    else:
+        print("there are NO updates on dev!!!")
+
+
+
 # enable station interface and connect to WiFi access point
 ssid="WONDERLAND"
 pw="Kiskutya9"
