824378a0f08954acd7f26752380e8746c49c9f7e
gamb1t <rawshiet@gmail.com>
2022-06-18 22:23:42 +0200

    ez lemaradt

--- main/parts/customtime.py
+++ main/parts/customtime.py
@@ -6,5 +6,6 @@ def printtime():
     print("Local time before synchronization：%s" %str(time.localtime()))
 
     time.sleep(3)
+    ntptime.settime()
 
     print("Local time after synchronization：%s" %str(time.localtime()))
