d89ba8e3aeadbcc2a6fb94b716c6d6358df39360
gamb1t <rawshiet@gmail.com>
2022-06-18 23:51:27 +0200

    haha this is not working

--- main/parts/customtime.py
+++ main/parts/customtime.py
@@ -6,6 +6,6 @@ def printtime():
     print("Local time before synchronization：%s" %str(time.localtime()))
     
     time.sleep(3)
-    ntptime.settime(timezone=1, server = 'pool.ntp.org')
+    ntptime.settime(timezone=1,server = 'pool.ntp.org')
 
     print("Local time after synchronization：%s" %str(time.localtime()))
