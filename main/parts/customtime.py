fe5a3c70e5d42c8e015017892366237cd020f838
gamb1t <rawshiet@gmail.com>
2022-06-19 17:15:17 +0200

    mand this ota fetch sucks ass

--- main/parts/customtime.py
+++ main/parts/customtime.py
@@ -21,5 +21,5 @@ def printtime():
             continue
     #the dirty way: add 2 hours
     actual_time=time.localtime()[3]+2
-    print("Local time after sync and modified timezone：%s" %str(actual_time)
+    print("Local time after sync and modified timezone：%s" %str(actual_time))
 #fricking ota man
